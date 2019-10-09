d9d2da2c26b50d9a3171252054f2720641b5eae9
Maxzb <47795883+Maxzb@users.noreply.github.com>
2019-10-10 00:14:43 +0300

    FULL SITE

--- script/script.js
+++ script/script.js
@@ -7,3 +7,43 @@ function slowScroll(id) {
     }, 1000);
     return false;
 }
+
+//МОДАЛЬНОЕ ОКНО
+//открыть по кнопке
+$('.detail_1').click(function() {
+    $('header, main, footer').css('filter', 'blur(5px)');
+	$('.overlay-modal_1').fadeIn();
+	//$('body').css('overflow', 'hidden');
+});
+$('.detail_2').click(function() {
+    $('header, main, footer').css('filter', 'blur(5px)');
+	$('.overlay-modal_2').fadeIn();
+	//$('body').css('overflow', 'hidden');
+});
+$('.detail_3').click(function() {
+    $('header, main, footer').css('filter', 'blur(5px)');
+	$('.overlay-modal_3').fadeIn();
+	//$('body').css('overflow', 'hidden');
+});
+$('.detail_4').click(function() {
+    $('header, main, footer').css('filter', 'blur(5px)');
+	$('.overlay-modal_4').fadeIn();
+	//$('body').css('overflow', 'hidden');
+});
+
+//закрыть на ПОНЯТНО
+$('.close-modal').click(function() {
+	$('.overlay-modal').fadeOut();
+	$('header, main, footer').css('filter', 'none');
+	$('body').css('overflow', 'auto');
+});
+
+//закрыть по клику вне окна
+$(document).mouseup(function(e) {
+	var modal = $('.modal');
+	if (e.target!=modal[0]&&modal.has(e.target).length === 0) {
+		$('.overlay-modal').fadeOut();
+		$('header, main, footer').css('filter', 'none');
+		$('body').css('overflow', 'auto');
+	}
+});
